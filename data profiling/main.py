7eb0298245ec1bff5850d7285061c27b5a489658
Duncanyu <duncan.w.yu@gmail.com>
2024-11-01 12:12:48 -0400

    Add files via upload

---
+++ data profiling/main.py
@@ -0,0 +1,23 @@
+from pandas import *
+
+#Data quality assessment
+
+file_path = "sample_data/process.csv"
+df = read_csv(file_path)
+
+#print(df.head(4))
+
+def count_empty(row):
+    empty_columns = [col for col in df.columns if isna(row[col] or str(row[col]).strip == '')]
+    return len(empty_columns), ', '.join(empty_columns)
+
+df['No_of_EmptyCell'] = (df.apply(lambda row: count_empty(row)[0], axis = 1))
+df['List_of_Empty_Col_Name'] = (df.apply(lambda row: count_empty(row)[1], axis = 1))
+
+first_column = df.pop('No_of_EmptyCell')
+second_column = df.pop('List_of_Empty_Col_Name')
+
+df.insert(0, 'No_of_EmptyCell', first_column)
+df.insert(0, 'List_of_Empty_Col_Name', second_column)
+
+df.to_csv('processed.csv', index = False)
